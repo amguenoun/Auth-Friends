1061e85a73aeec159e30a2b506d653e2c009d845
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-17 16:31:20 -0400

    moved form to friendsList for state and made it functional

--- friends/src/components/AddFriendForm.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AddFriendForm = () => {
-    return (
-        <div>Friend Form Here</div>
-    )
-}
-
-export default AddFriendForm;
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import axiosWithAuth from '../utils/axiosWithAuth';
 
-import AddFriendForm from './addFriendForm';
 import Friend from './Friend';
 
 const FriendList = () => {
     const [friendList, setFriendList] = useState([]);
+    const [friend, setFriend] = useState({ name: '', age: '', email: '', id: '' })
 
     useEffect(() => {
         axiosWithAuth().get('/friends')
@@ -13,9 +13,29 @@ const FriendList = () => {
             .catch(err => console.log('Error: ', err));
     }, [])
 
+    const handleFriendChange = (e) => {
+        setFriend({
+            ...friend,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axiosWithAuth().post('/friends', { ...friend, id: Date.now() })
+            .then(res => setFriendList(res.data))
+            .catch(err => console.log('Error: ', err));
+        setFriend({ name: '', age: '', email: '', id: '' })
+    }
+    console.log(friendList);
     return (
         <div>
-            <AddFriendForm />
+            <form onSubmit={handleSubmit}>
+                <input type='text' name='name' placeholder='name' value={friend.name} onChange={handleFriendChange} />
+                <input type='text' name='age' placeholder='age' value={friend.age} onChange={handleFriendChange} />
+                <input type='text' name='email' placeholder='email' value={friend.email} onChange={handleFriendChange} />
+                <button type='submit'>Add Friend</button>
+            </form>
             {friendList.map(friend => <Friend key={friend.id} friend={friend} />)}
         </div>
     );
