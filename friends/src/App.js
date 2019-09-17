2e0acced45494665f0c35ae6c6849e19d4ca89fc
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-17 15:48:11 -0400

    PrivateRoute added that renders FriendList component

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import LoginForm from './components/LoginForm';
+import PrivateRoute from './components/PrivateRoute';
+import FriendList from './components/FriendsList';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Route exact path='/' component={LoginForm} />
+      <PrivateRoute path='/friends' component={FriendList} />
     </div>
   );
 }
