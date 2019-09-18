c08e7bae3a562bb08936e1ec11b5afe834534384
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-18 11:33:35 -0400

    passing props to component through the route render

--- friends/src/components/PrivateRoute.js
+++ friends/src/components/PrivateRoute.js
@@ -3,7 +3,7 @@ import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
     return (
-        <Route {...rest} render={() => localStorage.getItem('token') ? <Component /> : <Redirect to='/' />} />
+        <Route {...rest} render={(props) => localStorage.getItem('token') ? <Component {...props} /> : <Redirect to='/' />} />
     );
 }
 
